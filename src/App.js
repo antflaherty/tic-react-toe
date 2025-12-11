f1120c8632a1c4c312bf62a9e5d0beb704a9cf72
Anthony Flaherty <aflaherty@certinia.com>
2025-12-11 15:26:48 +0000

    Pass square value as a prop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-function Square() {
-  return <button className="square">X</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Grid() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="0,2" />
+        <Square value="1,2" />
+        <Square value="2,2" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="0,1" />
+        <Square value="1,1" />
+        <Square value="2,1" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="0,0" />
+        <Square value="1,0" />
+        <Square value="2,0" />
       </div>
     </>
   );
