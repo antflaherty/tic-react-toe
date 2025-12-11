d59342f418f5b7165e0cdd70e66f68f7feda7b01
Anthony Flaherty <aflaherty@certinia.com>
2025-12-11 16:11:04 +0000

    Lift state up to board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ handleClick, value }) {
   return (
     <button className="square" onClick={handleClick}>
       {value}
@@ -15,22 +9,61 @@ function Square() {
 }
 
 export default function Grid() {
+  const [boardState, setBoardState] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+
+  function handleSquareClick(row, column) {
+    const newBoardState = structuredClone(boardState);
+    newBoardState[row][column] = "X";
+    setBoardState(newBoardState);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          handleClick={() => handleSquareClick(0, 0)}
+          value={boardState[0][0]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(0, 1)}
+          value={boardState[0][1]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(0, 2)}
+          value={boardState[0][2]}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          handleClick={() => handleSquareClick(1, 0)}
+          value={boardState[1][0]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(1, 1)}
+          value={boardState[1][1]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(1, 2)}
+          value={boardState[1][2]}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          handleClick={() => handleSquareClick(2, 0)}
+          value={boardState[2][0]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(2, 1)}
+          value={boardState[2][1]}
+        />
+        <Square
+          handleClick={() => handleSquareClick(2, 2)}
+          value={boardState[2][2]}
+        />
       </div>
     </>
   );
